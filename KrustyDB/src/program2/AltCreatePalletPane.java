45942a58b137002666d9fb2517de954155f5869d
Kevin <KOOL_music@hotmail.com>
2017-04-16 20:52:16 +0200

    Lade till alklasser för fix

---
+++ KrustyDB/src/program2/AltCreatePalletPane.java
@@ -0,0 +1,176 @@
+package program2;
+
+import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+
+public class AltCreatePalletPane extends BasicPane {
+
+    private static final int ORDER_ID = 0;
+    private static final int ORDER_DATE = 1;
+    private static final int COMPANY_NAME = 2;
+    private static final int NBR_FIELDS = 3;
+    private JTextField[] fields;
+
+    private static final long serialVersionUID = 1;
+
+    private DefaultListModel<String> cookieListModel;
+    private JList<String> cookieList;
+
+    private DefaultListModel<Integer> orderIDListModel;
+    private JList<Integer> orderIDList;
+
+    public AltCreatePalletPane(Database db) {
+        super(db);
+    }
+
+    public JComponent createLeftPanel() {
+        cookieListModel = new DefaultListModel<String>();
+
+        cookieList = new JList<String>(cookieListModel);
+        cookieList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        cookieList.setPrototypeCellValue("123456789012");
+        cookieList.addListSelectionListener(new NameSelectionListener());
+        JScrollPane p1 = new JScrollPane(cookieList);
+
+        orderIDListModel = new DefaultListModel<Integer>();
+
+        orderIDList = new JList<Integer>(orderIDListModel);
+        orderIDList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        orderIDList.setPrototypeCellValue(Integer.MAX_VALUE);
+        orderIDList.addListSelectionListener(new NameSelectionListener());
+        JScrollPane p2 = new JScrollPane(orderIDList);
+
+
+        JPanel p = new JPanel();
+        p.setLayout(new GridLayout(1, 2));
+        p.add(p2);
+        p.add(p1);
+
+        return p;
+    }
+
+    public JComponent createTopPanel() {
+        String[] texts = new String[NBR_FIELDS];
+        texts[ORDER_ID] = "Order ID";
+        texts[ORDER_DATE] = "Order date";
+        texts[COMPANY_NAME] = "Company name";
+
+        fields = new JTextField[NBR_FIELDS];
+        for (int i = 0; i < fields.length; i++) {
+            fields[i] = new JTextField(20);
+            fields[i].setEditable(false);
+        }
+
+        JPanel input = new InputPanel(texts, fields);
+
+        JPanel p = new JPanel();
+        p.setLayout(new BoxLayout(p, BoxLayout.Y_AXIS));
+        p.add(input);
+        return p;
+    }
+
+    public JComponent createBottomPanel() {
+        JButton[] buttons = new JButton[1];
+        buttons[0] = new JButton("Create pallet");
+        return new ButtonAndMessagePanel(buttons, messageLabel, new ActionHandler());
+    }
+
+
+    private void fillOrderIDList() {
+        orderIDListModel.removeAllElements();
+        ArrayList<Integer> orderIDs = db.getOrderID();
+
+        if(orderIDs != null){
+            for( Integer s : orderIDs) {
+                orderIDListModel.addElement(s);
+
+            }
+        }
+    }
+
+    private void fillCookieList() {
+        cookieListModel.removeAllElements();
+        ArrayList<String> cookies = db.getCookieNames();
+
+        if(cookies != null){
+            for(String s : cookies) {
+                cookieListModel.addElement(s);
+
+            }
+        }
+    }
+
+    public void entryActions() {
+        clearMessage();
+        fillOrderIDList();
+        fillCookieList();
+        clearFields();
+    }
+
+    private void clearFields() {
+        for (int i = 0; i < fields.length; i++) {
+            fields[i].setText("");
+        }
+    }
+
+    class NameSelectionListener implements ListSelectionListener {
+        /**
+         * Called when the user selects a name in the name list. Fetches
+         * performance dates from the database and displays them in the date
+         * list.
+         *
+         * @param e
+         *            The selected list item.
+         */
+        public void valueChanged(ListSelectionEvent e) {
+            if (orderIDList.isSelectionEmpty()) {
+                return;
+            }
+            int orderID = orderIDList.getSelectedValue();
+			/* --- insert own code here --- */
+            fields[ORDER_ID].setText(orderID + "");
+            fields[ORDER_DATE].setText(db.getOrderDate(orderID) + "");
+            fields[COMPANY_NAME].setText(db.getCompanyName(orderID) + "");
+        }
+    }
+
+    class ActionHandler implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if (orderIDList.isSelectionEmpty() || cookieList.isSelectionEmpty()) {
+                return;
+            }
+
+            int orderID = orderIDList.getSelectedValue();
+            String cookie = cookieList.getSelectedValue();
+            ArrayList<String> ingredients = db.getRecipe(cookieList.getSelectedValue());
+
+            if(db.ifBlocked(cookie) == true){
+                messageLabel.setText("This cookie is blocked!");
+                return;
+            }
+
+            for(String s : ingredients){
+                if(db.getIngredientAmountToUse(s) > db.getIngredientInStock(s)){
+                    messageLabel.setText("Not enough ingredients to make pallet!");
+                    return;
+                }
+            }
+
+            messageLabel.setText("Pallet created!");
+            db.makePallet(orderID, cookie);
+            for(String s : ingredients){
+               db.decreaseIngredient(s, db.getIngredientAmountToUse(s));
+            }
+
+            }
+
+        }
+    }
---
+++ KrustyDB/src/program2/AltDatabase.java
@@ -0,0 +1,678 @@
+package program2;
+
+//package app;
+
+import java.sql.*;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+//import dbtLab3.*;
+
+/**
+ * Database is a class that specifies the interface to the movie database. Uses
+ * JDBC.
+ */
+public class AltDatabase {
+
+	/**
+	 * The database connection.
+	 */
+	private Connection conn;
+		/**
+	 * Create the database interface object. Connection to the database is
+	 * performed later.
+	 */
+	public AltDatabase() {
+		conn = null;
+	}
+
+	/**
+	 * Open a connection to the database, using the specified user name and
+	 * password.
+	 */
+	public boolean openConnection(String filename) {
+		try {
+			Class.forName("org.sqlite.JDBC");
+			conn = DriverManager.getConnection("jdbc:sqlite:" + filename);
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+
+	/**
+	 * Close the connection to the database.
+	 */
+	public void closeConnection() {
+		try {
+			if (conn != null) {
+				conn.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Check if the connection to the database has been established
+	 * 
+	 * @return true if the connection has been established
+	 */
+	public boolean isConnected() {
+		return conn != null;
+	}
+
+	/* --- insert own code here --- */
+	/**
+	 * Gets the information about a specific pallet.
+	 * 
+	 * @param id
+	 * @return list of all info.
+	 */
+	public ArrayList<Integer> getId(String cookie, String date) {
+		ArrayList<Integer> pallets = new ArrayList<Integer>();
+		PreparedStatement ps = null;
+
+		try {
+			String sql = "SELECT id FROM pallets where cookie_name = ? and production_date = ?";
+			ps = conn.prepareStatement(sql);
+			ps.setString(1, cookie);
+			ps.setString(2, date);
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()) {
+				pallets.add(rs.getInt("id"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (ps != null) {
+				try {
+					ps.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return pallets;
+	}
+
+	/**
+	 * Gets blocked cookies from database.
+	 * 
+	 * @return list of blocked cookies
+	 */
+	public ArrayList<String> getBlockedCookies() {
+		try {
+			String sql = "SELECT * from Cookie where isBlocked = true";
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ResultSet rs = ps.executeQuery();
+
+			ArrayList<String> list = new ArrayList<>();
+
+			while (rs.next()) {
+				list.add(rs.getString("name"));
+			}
+
+			return list;
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+
+	/**
+	 * Get blocked pallets of a specific cookie.
+	 * 
+	 * @param cookie
+	 * @return list of blocked pallets.
+	 */
+	public ArrayList<String> getBlockedPallets(String cookie) {
+		try {
+			String sql = "SELECT * from Cookie natural join Pallet where Cookie.isBlocked = true and Pallet.cookieName = ? and Cookie.name = Pallet.cookieName";
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setString(1, cookie);
+			ResultSet rs = ps.executeQuery();
+
+			ArrayList<String> list = new ArrayList<>();
+
+			while (rs.next()) {
+				list.add(Integer.toString(rs.getInt("ID")));
+			}
+
+			return list;
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+
+	public String getPalletLocation(int id) {
+		String location = "test";
+		PreparedStatement ps = null;
+
+		try {
+			String sql = "SELECT location FROM pallets WHERE ID = ?";
+			ps = conn.prepareStatement(sql);
+			ps.setInt(1, id);
+			ResultSet rs = ps.executeQuery();
+			rs.next();
+			location = rs.getString("location");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (ps != null) {
+				try {
+					ps.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return location;
+	}
+
+	public ArrayList<Integer> getPalletID(String cookie, String date) {
+		ArrayList<Integer> pallets = new ArrayList<Integer>();
+		PreparedStatement ps = null;
+
+		try {
+			String sql = "SELECT id FROM pallets WHERE cookie_name = ? and production_date = ?";
+			ps = conn.prepareStatement(sql);
+			ps.setString(1, cookie);
+			ps.setString(2, date);
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()) {
+				pallets.add(rs.getInt("id"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (ps != null) {
+				try {
+					ps.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return pallets;
+	}
+
+	/**
+	 * 
+	 * @return cookieNames
+	 */
+	public ArrayList<String> getCookieNames() {
+		ArrayList<String> cookies = new ArrayList<String>();
+		PreparedStatement ps = null;
+
+		try {
+			String sql = "SELECT name FROM cookies";
+			ps = conn.prepareStatement(sql);
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()) {
+				cookies.add(rs.getString("name"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (ps != null) {
+				try {
+					ps.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return cookies;
+	}
+
+	public ArrayList<String> getCreatedDates(String cookie) {
+		ArrayList<String> dates = new ArrayList<String>();
+		PreparedStatement ps = null;
+
+		try {
+			String sql = "SELECT DISTINCT production_date FROM pallets WHERE cookie_name = ?";
+			ps = conn.prepareStatement(sql);
+			ps.setString(1, cookie);
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()) {
+				dates.add(rs.getString("production_date"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (ps != null) {
+				try {
+					ps.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return dates;
+	}
+
+	public String getCreatedDate(int id) {
+		PreparedStatement ps = null;
+		String date = null;
+		try {
+			String sql = "SELECT production_date FROM pallets WHERE ID = ?";
+			ps = conn.prepareStatement(sql);
+			ps.setInt(1, id);
+			ResultSet rs = ps.executeQuery();
+			rs.next();
+			date = rs.getString("production_date");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (ps != null) {
+				try {
+					ps.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return date;
+	}
+
+	public boolean getPalletifDelivered(int id) {
+		boolean isDelivered = false;
+		PreparedStatement ps = null;
+
+		try {
+			String sql = "SELECT isDelivered FROM pallets WHERE ID = ?";
+			ps = conn.prepareStatement(sql);
+			ps.setInt(1, id);
+			ResultSet rs = ps.executeQuery();
+			rs.next();
+			isDelivered = rs.getBoolean("isDelivered");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (ps != null) {
+				try {
+					ps.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return isDelivered;
+	}
+
+	public String getPalletdDate(int id) {
+		String dDate = "test";
+		PreparedStatement ps = null;
+
+		try {
+			String sql = "SELECT delivery_date FROM pallets WHERE ID = ?";
+			ps = conn.prepareStatement(sql);
+			ps.setInt(1, id);
+			ResultSet rs = ps.executeQuery();
+			rs.next();
+			dDate = rs.getString("delivery_date");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (ps != null) {
+				try {
+					ps.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		return dDate;
+	}
+
+	public int getPalletOrderID(int id) {
+		int orderID = 0;
+		PreparedStatement ps = null;
+
+		try {
+			String sql = "SELECT orderID FROM pallets WHERE ID = ?";
+			ps = conn.prepareStatement(sql);
+			ps.setInt(1, id);
+			ResultSet rs = ps.executeQuery();
+			rs.next();
+			orderID = rs.getInt("orderID");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (ps != null) {
+				try {
+					ps.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return orderID;
+	}
+
+	public Boolean ifBlocked(String cookie) {
+		PreparedStatement ps = null;
+		Boolean block = false;
+		try {
+			String sql = "SELECT isBlocked FROM cookies WHERE name = ?";
+			ps = conn.prepareStatement(sql);
+			ps.setString(1, cookie);
+			ResultSet rs = ps.executeQuery();
+			rs.next();
+			block = rs.getBoolean("isBlocked");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (ps != null) {
+				try {
+					ps.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return block;
+	}
+
+	/**
+	 * Booleans i sql är integer 1 eller 0
+	 * 
+	 * @param cookie
+	 */
+	public void blockCookie(String cookie) {
+		PreparedStatement ps = null;
+
+		try {
+			String sql = "UPDATE cookies SET isBlocked = 1 where name = ?";
+			ps = conn.prepareStatement(sql);
+			ps.setString(1, cookie);
+			ps.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (ps != null) {
+				try {
+					ps.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+	}
+
+	public void unBlockCookie(String cookie) {
+		PreparedStatement ps = null;
+
+		try {
+			String sql = "UPDATE cookies SET isBlocked = 0 WHERE name = ?";
+			ps = conn.prepareStatement(sql);
+			ps.setString(1, cookie);
+			ps.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (ps != null) {
+				try {
+					ps.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+	public ArrayList<Integer> getOrderID() {
+		ArrayList<Integer> orderID = new ArrayList<Integer>();
+		PreparedStatement ps = null;
+
+		try {
+			String sql = "SELECT ID FROM orders";
+			ps = conn.prepareStatement(sql);
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()) {
+				orderID.add(rs.getInt("id"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (ps != null) {
+				try {
+					ps.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return orderID;
+	}
+
+	public String getOrderDate(int orderID) {
+		String orderDate = "test";
+		PreparedStatement ps = null;
+
+		try {
+			String sql = "SELECT orderDate FROM orders WHERE ID = ?";
+			ps = conn.prepareStatement(sql);
+			ps.setInt(1, orderID);
+			ResultSet rs = ps.executeQuery();
+			rs.next();
+			orderDate = rs.getString("orderDate");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (ps != null) {
+				try {
+					ps.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return orderDate;
+	}
+
+	public String getCompanyName(int orderID) {
+		String companyName = "test";
+		PreparedStatement ps = null;
+
+		try {
+			String sql = "SELECT customer_name FROM orders WHERE ID = ?";
+			ps = conn.prepareStatement(sql);
+			ps.setInt(1, orderID);
+			ResultSet rs = ps.executeQuery();
+			rs.next();
+			companyName = rs.getString("customer_name");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (ps != null) {
+				try {
+					ps.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return companyName;
+	}
+
+	public ArrayList<String> getRecipe(String cookie) {
+		ArrayList<String> ingredients = new ArrayList<String>();
+		PreparedStatement ps = null;
+
+		try {
+			String sql = "SELECT ingredient_name FROM recipes WHERE cookie_name = ?";
+			ps = conn.prepareStatement(sql);
+			ps.setString(1, cookie);
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()) {
+				ingredients.add(rs.getString("ingredient_name"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (ps != null) {
+				try {
+					ps.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return ingredients;
+	}
+
+	public float getIngredientAmountToUse(String ingredient) {
+		float amount = 0;
+		PreparedStatement ps = null;
+
+		try {
+			String sql = "SELECT amount FROM recipes WHERE ingredient_name = ?";
+			ps = conn.prepareStatement(sql);
+			ps.setString(1, ingredient);
+			ResultSet rs = ps.executeQuery();
+			rs.next();
+			amount = rs.getFloat("amount");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (ps != null) {
+				try {
+					ps.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return amount;
+	}
+
+	public float getIngredientInStock(String ingredient) {
+		float amount = 0;
+		PreparedStatement ps = null;
+
+		try {
+			String sql = "SELECT stock FROM ingredients WHERE name = ?";
+			ps = conn.prepareStatement(sql);
+			ps.setString(1, ingredient);
+			ResultSet rs = ps.executeQuery();
+			rs.next();
+			amount = rs.getFloat("stock");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (ps != null) {
+				try {
+					ps.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return amount;
+	}
+
+	public void makePallet(int orderID, String cookie_name) {
+		String defaultLocation = "Production";
+		boolean isDelivered = false;
+		String deliveryDate = null;
+		boolean isBlocked = false;
+		String production_date = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+
+		ResultSet rs;
+		PreparedStatement ps = null;
+		Savepoint savepoint = null;
+
+		try {
+			String sql = "INSERT INTO Pallets(production_date, cookie_name, location, orderID, delivery_date, isDelivered, isPalletBlocked) VALUES(?, ?, ?, ?, ?, ?, ?)";
+			ps = conn.prepareStatement(sql);
+			//ps.setInt(1, 0);
+			ps.setString(1, production_date);
+			ps.setString(2, cookie_name);
+			ps.setString(3, defaultLocation);
+			ps.setInt(4, orderID);
+			ps.setString(5, deliveryDate);
+			ps.setBoolean(6, isDelivered);
+			ps.setBoolean(7, isBlocked);
+
+			ps.executeUpdate();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (ps != null) {
+				try {
+					ps.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+	public void decreaseIngredient(String ingredient, float amount) {
+
+		PreparedStatement ps = null;
+
+		try {
+			String sql = "UPDATE ingredients SET stock = stock - ? WHERE name = ?";
+			ps = conn.prepareStatement(sql);
+			ps.setFloat(1, amount);
+			ps.setString(2, ingredient);
+
+			ps.executeUpdate();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (ps != null) {
+				try {
+					ps.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+	}
+
+	public ArrayList<String> getBlockedProducts() {
+		PreparedStatement ps = null;
+		ArrayList<String> cookies = new ArrayList<String>();
+		String sql = "SELECT name FROM cookies WHERE isBlocked = 1";
+		try {
+			ps = conn.prepareStatement(sql);
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()) {
+				String cookieName = rs.getString("name");
+				cookies.add(cookieName + "\n");
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (ps != null) {
+				try {
+					ps.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return cookies;
+
+	}
+
+}
